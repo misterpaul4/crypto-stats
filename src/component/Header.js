7e6f3f16172d25eec7dcee33ee15319e7a5f0626
paul <ajuzepaul@gmail.com>
2021-02-12 22:26:08 +0200

    Add App logo

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/header.css';
 
 const Header = () => (
-  <header>
-    <Link to="/">Logo</Link>
+  <header className="flex-around">
+    <Link to="/" className="flex-centered home">
+      <div className="logo bg-img" />
+      <span className="logo-name">Crypto Stats</span>
+    </Link>
   </header>
 );
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../component/App';
 import Header from '../component/Header';
+import '../css/index.css';
 
 const Routes = () => (
   <BrowserRouter>
