bfff7f1e6abbab11471ab543af69eff004e30562
paul <ajuzepaul@gmail.com>
2021-02-15 03:30:21 +0200

    Add icons to crypto and format prices and percentages

--- src/component/Crypto.js
+++ src/component/Crypto.js
@@ -4,28 +4,39 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const Crypto = props => (
-  <tr className="chart-crypto-row">
-    <td>{props.crypto.market_cap_rank}</td>
-    <td>{props.crypto.name}</td>
-    <td>
-      $
-      {props.crypto.current_price}
-    </td>
-    <td>
-      {props.crypto.price_change_percentage_7d_in_currency}
-      %
-    </td>
-    <td>
-      {props.crypto.price_change_percentage_24h}
-      %
-    </td>
-    <td>$100 7days ago</td>
-    <td>
-      $
-      {props.crypto.market_cap}
-    </td>
-  </tr>
-);
+const Crypto = props => {
+  const to2Decimal = value => parseFloat(value).toFixed(2);
 
+  const percentTD = value => (
+    <td className={value > '0' ? 'text-success' : 'text-danger'}>
+      {to2Decimal(value)}
+      %
+    </td>
+  );
+
+  const moneyWithCommas = amount => amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  return (
+    <tr className="chart-crypto-row">
+      <td>{props.crypto.market_cap_rank}</td>
+      <td>
+        <img src={props.crypto.image} alt="icon" className="mr-2" />
+        {props.crypto.name}
+      </td>
+      <td>
+        $
+        {moneyWithCommas(to2Decimal(props.crypto.current_price))}
+      </td>
+
+      {percentTD(props.crypto.price_change_percentage_7d_in_currency)}
+      {percentTD(props.crypto.price_change_percentage_24h)}
+
+      <td>$100 7days ago</td>
+      <td>
+        $
+        {moneyWithCommas(props.crypto.market_cap)}
+      </td>
+    </tr>
+  );
+};
 export default Crypto;
--- src/containers/Chart.js
+++ src/containers/Chart.js
@@ -8,59 +8,6 @@ import '../css/chart.css';
 import Crypto from '../component/Crypto';
 
 const Chart = props => {
-  const temp = {
-    cryptos: [
-      {
-        rank: 1,
-        name: 'dogecoin',
-        price: '$0.054',
-        d7: '3.2%',
-        h24: '5.7%',
-        cap: '$600,000',
-      },
-      {
-        rank: 2,
-        name: 'bitcoin',
-        price: '$47,000',
-        d7: '2.28%',
-        h24: '15.7%',
-        cap: '$600,580,000',
-      },
-      {
-        rank: 3,
-        name: 'ripple',
-        price: '$0.54',
-        d7: '-5.2%',
-        h24: '0.7%',
-        cap: '$1,600,000',
-      },
-      {
-        rank: 4,
-        name: 'etherium',
-        price: '$1,800',
-        d7: '7.2%',
-        h24: '75.7%',
-        cap: '$5,600,000',
-      },
-      {
-        rank: 5,
-        name: 'monero',
-        price: '$154',
-        d7: '8.2%',
-        h24: '5.7%',
-        cap: '$600,000',
-      },
-      {
-        rank: 6,
-        name: 'tatcoin',
-        price: '$54',
-        d7: '60%',
-        h24: '51.7%',
-        cap: '$60,000',
-      },
-    ],
-  };
-
   const passCrypto = crypto => (<Crypto crypto={crypto} key={crypto.name} />);
 
   return (
