e98e8fb4b7048239dd665712dbb7a08a54808031
Paul Ajuizeogu <ajuzepaul@gmail.com>
2023-01-07 18:09:15 +0200

    Add active class on headers

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -1,34 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../../css/header.css";
 import { Button, Input, Space } from "antd";
 import { FcLike } from "react-icons/fc";
 import { PATHS } from "../../paths";
 
-const Header = () => (
-  <header className="py-2 px-5">
-    <Link to="/">
-      <div className="logo bg-img" />
-    </Link>
+const Header = () => {
+  const pathname = useLocation().pathname;
 
-    <Space size="large">
-      <Link to="/">
-        <Button type="text">Cryptocurrencies</Button>
-      </Link>
-      <Link to="/">
-        <Button type="text">Exchanges</Button>
-      </Link>
-    </Space>
+  const activeClass = (path) => (pathname === path ? "active-header" : "");
 
-    <Space size="large">
-      <Link to={PATHS.favourites}>
-        <Button icon={<FcLike className="mr-2" />} type="text">
-          Favourites
-        </Button>
+  return (
+    <header className="py-2 px-5">
+      <Link to="/">
+        <div className="logo bg-img" />
       </Link>
-      <Input.Search allowClear placeholder="Search" />
-    </Space>
-  </header>
-);
+
+      <Space size="large">
+        <Link to="/">
+          <Button type="text" className={activeClass(PATHS.home)}>
+            Cryptocurrencies
+          </Button>
+        </Link>
+        <Link to="/">
+          <Button className={activeClass(PATHS.exchanges)} type="text">
+            Exchanges
+          </Button>
+        </Link>
+      </Space>
+
+      <Space size="large">
+        <Link to={PATHS.favourites}>
+          <Button
+            className={activeClass(PATHS.favourites)}
+            icon={<FcLike className="mr-2" />}
+            type="text"
+          >
+            Favourites
+          </Button>
+        </Link>
+        <Input.Search allowClear placeholder="Search" />
+      </Space>
+    </header>
+  );
+};
 
 export default Header;
--- src/paths.js
+++ src/paths.js
@@ -1,4 +1,5 @@
 export const PATHS = {
   home: "/",
   favourites: "/favourites",
+  exchanges: "/exchanges",
 };
