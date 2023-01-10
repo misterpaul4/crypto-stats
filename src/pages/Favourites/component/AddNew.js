4ca82c09dec6577a935a2ffe41a1f3feb39c1159
Paul Ajuizeogu <ajuzepaul@gmail.com>
2023-01-10 03:04:19 +0200

    Add search and data

--- src/pages/Favourites/component/AddNew.js
+++ src/pages/Favourites/component/AddNew.js
@@ -1,9 +1,81 @@
-import { Modal } from "antd";
+import { Avatar, Button, Input, List, Modal, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { FcLike } from "react-icons/fc";
 
-const AddNewFavourite = ({ favourites, visibility, onClose }) => {
+const AddNewFavourite = ({
+  favourites,
+  visibility,
+  onClose,
+  loading,
+  data,
+}) => {
+  const [selectedCoins, setSelectedCoins] = useState([]);
+  const [filteredData, setFilteredData] = useState(data || []);
+
+  useEffect(() => {
+    if (data) {
+      setFilteredData(data);
+    }
+  }, [data]);
+
+  const handleSearch = (searchTerm) => {
+    if (searchTerm) {
+      const filteredResult = data.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          coin.symbol.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          coin.id.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredData(filteredResult);
+    } else {
+      setFilteredData(data);
+    }
+  };
+
   return (
-    <Modal open={visibility} onCancel={onClose}>
-      modal content
+    <Modal
+      centered
+      cancelButtonProps={{ className: "d-none" }}
+      okButtonProps={{ disabled: !selectedCoins.length }}
+      title={
+        <div>
+          <Typography.Title className="text-center" level={3}>
+            Add Coins {!!selectedCoins.length && `[${selectedCoins.length}]`}
+          </Typography.Title>
+
+          <Input
+            onChange={(e) => handleSearch(e.target.value)}
+            placeholder="Search..."
+            allowClear
+            className="mt-2"
+          />
+        </div>
+      }
+      okText="Add Selected"
+      open={visibility}
+      onCancel={onClose}
+    >
+      <List
+        loading={loading}
+        className="add-favourite-list"
+        dataSource={filteredData}
+        renderItem={(coin) => (
+          <List.Item>
+            <div>
+              <Avatar size="small" src={coin.image} className="ml-1 mr-2" />
+              <strong>{coin.name}</strong>
+              <span className="ml-1 text-muted">
+                {coin.symbol.toUpperCase()}
+              </span>
+            </div>
+            <Button
+              className="flex-centered"
+              type="text"
+              icon={<FcLike />}
+            ></Button>
+          </List.Item>
+        )}
+      />
     </Modal>
   );
 };
--- src/pages/Favourites/index.js
+++ src/pages/Favourites/index.js
@@ -1,16 +1,24 @@
 import { useState } from "react";
+import useLazyAPI from "../../app/hooks/useLazyAPI";
+import { ALL_TOKENS } from "../../endpoints";
 import { getLs, LOCAL_STORAGE_KEYS } from "../../utils/localStorage";
 import AddNewFavourite from "./component/AddNew";
 import Favourites from "./component/Favourites";
 import NoFavourites from "./component/NoFavourites";
+import "../../css/favourite.css";
 
 const FavouritesPage = () => {
   const favourites = getLs(LOCAL_STORAGE_KEYS.favourites) || [];
+
+  const [getCoins, { loading, data }] = useLazyAPI({ url: ALL_TOKENS });
 
   // 'add new' modal
   const [visibility, setVisibility] = useState(false);
 
   const onModalOpen = () => {
+    if (!data) {
+      getCoins();
+    }
     setVisibility(true);
   };
 
@@ -21,6 +29,8 @@ const FavouritesPage = () => {
   return (
     <>
       <AddNewFavourite
+        loading={loading}
+        data={data}
         onClose={onModalClose}
         visibility={visibility}
         favourites={favourites}
