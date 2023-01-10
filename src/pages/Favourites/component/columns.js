18352035361658175e49d5da49cdd5a33d3c9e58
Paul Ajuizeogu <ajuzepaul@gmail.com>
2023-01-10 06:33:08 +0200

    Update

--- src/pages/Favourites/component/columns.js
+++ src/pages/Favourites/component/columns.js
@@ -1,27 +1,32 @@
 import { Button } from "antd";
+import { useContext } from "react";
 import { FcDislike } from "react-icons/fc";
-import { removeFromFavourites } from "../../../app/helpers/localStorageActions";
 import { removeSucesss } from "../../../app/helpers/message";
 import { commonColumns } from "../../Cryptocurrencies/component/columns";
+import { FavouriteContext } from "../context/favouriteContext";
 
-const columns = () => [
-  ...commonColumns,
-  {
-    title: "Action",
-    fixed: "right",
-    render: (data) => (
-      <Button
-        type="text"
-        onClick={() => {
-          removeFromFavourites([data.id]);
-          removeSucesss(data.symbol);
-        }}
-      >
-        <FcDislike className="mr-2" /> Remove
-        <strong className="mx-1">{data.symbol.toUpperCase()}</strong>
-      </Button>
-    ),
-  },
-];
+const columns = () => {
+  const { onFavouriteUpdate, favourites } = useContext(FavouriteContext);
+
+  return [
+    ...commonColumns,
+    {
+      title: "Action",
+      fixed: "right",
+      render: (data) => (
+        <Button
+          type="text"
+          onClick={() => {
+            onFavouriteUpdate(favourites.filter((id) => id !== data.id));
+            removeSucesss(data.symbol);
+          }}
+        >
+          <FcDislike className="mr-2" /> Remove
+          <strong className="mx-1">{data.symbol.toUpperCase()}</strong>
+        </Button>
+      ),
+    },
+  ];
+};
 
 export default columns;
